22f73c8c35e88ac016ba83c988dbed1aa5a36053
Clemence Granade <clemencegranade@Clemences-MacBook-Air.local>
2022-10-10 21:13:07 -0400

    cleaning file name update

--- ift6758/data/__init__.py
+++ ift6758/data/__init__.py
@@ -13,4 +13,4 @@ https://docs.python.org/3/reference/import.html#regular-packages
 """
 from .extractor import NHLExtractor
 from .loader import NHLLoader
-from .converter import NHLConverter
+from .converter import NHLCleaner
