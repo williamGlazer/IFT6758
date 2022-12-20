94b6bf55135b952ff29ede5d70e3fd9e8cb4a909
williamGlazer <willcavanagh@hotmail.com>
2022-12-20 17:19:43 -0500

    added streamlit skeleton

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,36 +1,29 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 
-st.title('Uber pickups in NYC')
+st.title('Hockey Match Prediction')
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+# SIDEBAR
+with st.sidebar:
+    workspace = st.text_input('Workspace', value='williamglazer')
+    model = st.text_input('Model', value='naive-bayes')
+    version = st.text_input('Version', value='1.0.0')
 
-@st.cache
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+    if st.button('load model'):
+        # trigger post request to container
+        # animation upon loading
+        # validate if model exists
+        # if not warn
+        # if success display load successfull
+        pass
 
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache)")
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
+# BODY
+id = st.text_input('game id')
 
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
-
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+if st.button('evaluate game'):
+    # trigger post request to container
+    # animation upon loading
+    # validate if game id is legal
+    # if not warn
+    # if yes display results and dataframe
+    pass
